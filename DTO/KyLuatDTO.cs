1a3f2022998b0df48bce2044239efe7835228a09
cannhiala <cannhiala@gmail.com>
2023-06-13 22:45:49 +0700

    Add files via upload

---
+++ DTO/KyLuatDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace DTO
+{
+    public class KyLuatDTO
+    {
+        private string maNamHoc;
+        private string maLop;
+        private string maHocSinh;
+        private string hinhThuc;
+        private string noiDung;
+
+        public KyLuatDTO(string maNamHoc, string maLop, string maHocSinh, string hinhThuc, string noiDung)
+        {
+            this.maNamHoc = maNamHoc;
+            this.maLop = maLop;
+            this.maHocSinh = maHocSinh;
+            this.hinhThuc = hinhThuc;
+            this.noiDung = noiDung;
+
+        }
+
+        public string MaNamHoc { get => maNamHoc; set => maNamHoc = value; }
+        public string MaLop { get => maLop; set => maLop = value; }
+        public string MaHocSinh { get => maHocSinh; set => maHocSinh = value; }
+        public string HinhThuc { get => hinhThuc; set => hinhThuc = value; }
+        public string NoiDung { get => noiDung; set => noiDung = value; }
+    }
+}
