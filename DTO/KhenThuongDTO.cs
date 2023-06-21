0f4acac2841d051a8cc86885233716237fce80ff
cannhiala <cannhiala@gmail.com>
2023-06-21 23:32:42 +0700

    Add files via upload

--- DTO/KhenThuongDTO.cs
+++ DTO/KhenThuongDTO.cs
@@ -7,21 +7,23 @@
         private string maHocSinh;
         private string hinhThuc;
         private string noiDung;
+        private string maKhoiLop;
 
-        public KhenThuongDTO(string maNamHoc, string maLop, string maHocSinh, string hinhThuc, string noiDung)
+        public KhenThuongDTO(string maNamHoc, string maLop, string maHocSinh, string hinhThuc, string noiDung, string maKhoiLop)
         {
             this.maNamHoc = maNamHoc;
             this.maLop = maLop;
             this.maHocSinh = maHocSinh;
             this.hinhThuc = hinhThuc;
             this.noiDung = noiDung;
-
-        }
+            this.maKhoiLop = maKhoiLop;
+    }
 
         public string MaNamHoc { get => maNamHoc; set => maNamHoc = value; }
         public string MaLop { get => maLop; set => maLop = value; }
         public string MaHocSinh { get => maHocSinh; set => maHocSinh = value; }
         public string HinhThuc { get => hinhThuc; set => hinhThuc = value; }
         public string NoiDung { get => noiDung; set => noiDung = value; }
+        public string MaKhoiLop { get => maKhoiLop; set => maKhoiLop = value; }
     }
 }
--- DTO/KyLuatDTO.cs
+++ DTO/KyLuatDTO.cs
@@ -7,15 +7,16 @@
         private string maHocSinh;
         private string hinhThuc;
         private string noiDung;
+        private string maKhoiLop;
 
-        public KyLuatDTO(string maNamHoc, string maLop, string maHocSinh, string hinhThuc, string noiDung)
+        public KyLuatDTO(string maNamHoc, string maLop, string maHocSinh, string hinhThuc, string noiDung, string maKhoiLop)
         {
             this.maNamHoc = maNamHoc;
             this.maLop = maLop;
             this.maHocSinh = maHocSinh;
             this.hinhThuc = hinhThuc;
             this.noiDung = noiDung;
-
+            this.maKhoiLop = maKhoiLop;
         }
 
         public string MaNamHoc { get => maNamHoc; set => maNamHoc = value; }
@@ -23,5 +24,6 @@
         public string MaHocSinh { get => maHocSinh; set => maHocSinh = value; }
         public string HinhThuc { get => hinhThuc; set => hinhThuc = value; }
         public string NoiDung { get => noiDung; set => noiDung = value; }
+        public string MaKhoiLop { get => maKhoiLop; set => maKhoiLop = value; }
     }
 }
